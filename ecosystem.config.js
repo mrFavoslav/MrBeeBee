64ad5c24db23e2643fd7dee494504b13b04fa992
mrFavoslav <ochmel@favoslav.cz>
2025-06-12 15:19:00 +0200

    edit: exec. mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,8 @@ module.exports = {
   apps: [{
     name: 'PreRelease',
     script: '/home/mrbb_bot/index.js',
+    instances : "max",
+    exec_mode : "cluster",
     watch : false,
     cron_restart: '0 0 * * *',
   }]
